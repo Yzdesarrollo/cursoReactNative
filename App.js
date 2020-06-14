389c0d8ebd8721230e46351fbd5f18c7138660fc
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-14 18:50:23 -0500

    Agregando estilos

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App(){
   return(
-    <View style={styles.container}>
-      <Text>Hello World!...</Text>
+    <View style={estilos.container}>
+      <Text style={estilos.text}>Hello World!...</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red'
+  }
 });
