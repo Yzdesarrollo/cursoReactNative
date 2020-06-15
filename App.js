96e51ad8035c79945ad07ee3f49a295cffc88327
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-14 19:14:52 -0500

    flexDirection: column, row

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <View style={estilos.container}>
-      <Text style={estilos.text}>Hello World!...</Text>
+      <View style={estilos.v1}>
+        <Text style={estilos.text}>vista 1</Text>
+      </View>
+      <View style={estilos.v2}>
+        <Text style={estilos.text}>vista 2</Text>
+      </View>
+      <View style={estilos.v3}>
+        <Text style={estilos.text}>vista 3</Text>
+      </View>
     </View>
   );
 }
@@ -12,11 +20,28 @@ export default function App(){
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    flexDirection: 'column'
+  },
+  text: {
+    color: 'black'
+  },
+  v1: {
+    flex: 1,
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: 'red'
+  v2: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  v3: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
